3aa3231dbbd55d4b145f46fec581d360eaadaa4a
mehelmi <masciciku@gmail.com>
2023-12-08 14:28:38 +0700

    Update Revisi

--- resources/views/master/master_tujuan_kirim_raw_material/create.blade.php
+++ resources/views/master/master_tujuan_kirim_raw_material/create.blade.php
@@ -48,21 +48,6 @@
                                 @enderror
                             </div>
 
-                            <div class="form-group">
-                                <label class="font-weight-bold">Status</label>
-                                {{-- <select name="" id=""></select> --}}
-                                <input type="text" class="form-control @error('status') is-invalid @enderror"
-                                    name="status" placeholder="Masukan Status">
-
-                                <!-- error message untuk title -->
-                                @error('status')
-                                    <div class="alert alert-danger mt-2">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-
-
                             <button type="submit" class="btn btn-md btn-primary">SIMPAN</button>
                             <button type="reset" class="btn btn-md btn-warning">RESET</button>
 
