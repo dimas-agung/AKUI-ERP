98c25677ca8677a8b17b46f2675956a57c68736a
mehelmi <masciciku@gmail.com>
2023-12-08 14:40:41 +0700

    Update Revisian

--- database/seeders/MasterTujuanKirimRawMaterialSeeder.php
+++ database/seeders/MasterTujuanKirimRawMaterialSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\MasterTujuanKirimRawMaterial;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class MasterTujuanKirimRawMaterialSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        MasterTujuanKirimRawMaterial::create([
+            'tujuan_kirim' => 'Surabaya',
+            'letak_tujuan' => 'Gudang 1',
+            'inisial_tujuan' => 'K001',
+            'status' => 1,
+        ]);
+        MasterTujuanKirimRawMaterial::create([
+            'tujuan_kirim' => 'Singapura',
+            'letak_tujuan' => 'Gudang 2',
+            'inisial_tujuan' => 'K002',
+            'status' => 1,
+        ]);
+        MasterTujuanKirimRawMaterial::create([
+            'tujuan_kirim' => 'Malaysia',
+            'letak_tujuan' => 'Gudang 3',
+            'inisial_tujuan' => 'K003',
+            'status' => 1,
+        ]);
     }
 }
