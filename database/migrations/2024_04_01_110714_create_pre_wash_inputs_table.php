3652b1e9363424814a83cea09f4c1522cd7ed4de
mehelmi <masciciku@gmail.com>
2024-05-28 11:11:26 +0700

    Menambahakan Kolom Status Pada PreWashInput

--- database/migrations/2024_04_01_110714_create_pre_wash_inputs_table.php
+++ database/migrations/2024_04_01_110714_create_pre_wash_inputs_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('tujuan_kirim');
             $table->string('keterangan')->nullable();
             $table->string('nomor_bstb');
-            $table->integer('status')->default(1);
             $table->float('modal', 16, 4);
             $table->float('total_modal', 16, 4);
             $table->string('user_created');
