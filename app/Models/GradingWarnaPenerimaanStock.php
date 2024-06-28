e08cecbbc750eb1fff45d2563a5f28c6ca792d71
Aelahyar <syaugialhyar@gmail.com>
2024-06-28 09:47:44 +0700

    menambahkan migration and model grading warna

---
+++ app/Models/GradingWarnaPenerimaanStock.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GradingWarnaPenerimaanStock extends Model
+{
+    use HasFactory;
+    protected $table = 'grading_warna_penerimaan_stocks';
+    protected $fillable = [
+        'nomor_job',
+        'nomor_bstb',
+        'nomor_batch',
+        'tujuan_kirim',
+        'keterangan',
+        'berat_kotor',
+        'jenis_grading',
+        'berat_1_grading',
+        'pcs_1_grading',
+        'berat_2_grading',
+        'modal',
+        'total_modal',
+        'status',
+    ];
+}
