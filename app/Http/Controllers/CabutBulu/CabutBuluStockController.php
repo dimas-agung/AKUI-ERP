d701f46711008751aac3b2250ee9563b53d3d73c
dimas agung <agungdimas390@gmail.com>
2024-07-04 08:14:45 +0700

    add dilter plant

--- app/Http/Controllers/CabutBulu/CabutBuluStockController.php
+++ app/Http/Controllers/CabutBulu/CabutBuluStockController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers\CabutBulu;
 use App\Http\Controllers\Controller;
 use App\Models\CabutBuluStock;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CabutBuluStockController extends Controller
 {
 
     public function index(){
         $i =1;
-        $cabut_bulu_stock = CabutBuluStock::where('status','!=',CabutBuluStock::STATUS_FINISHED)->get();
-
+        $cabut_bulu_stock = CabutBuluStock::where('status','!=',CabutBuluStock::STATUS_FINISHED);
+        if(Auth::user()->plant){
+            $cabut_bulu_stock->where('tujuan_kirim',Auth::user()->plant);
+        }
+        $cabut_bulu_stock = $cabut_bulu_stock->get();
         return response()->view('CabutBulu.CabutBuluStock.index', [
             'cabut_bulu_stock' => $cabut_bulu_stock,
         ]);
