19fd3b752601bfbff3482c487f7bf52dbd75ccf6
dimas agung <agungdimas390@gmail.com>
2024-07-04 08:14:56 +0700

    add filter plant

--- app/Http/Controllers/CabutBulu/TransitCabutBuluController.php
+++ app/Http/Controllers/CabutBulu/TransitCabutBuluController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\CabutBulu;
 use App\Http\Controllers\Controller;
 use App\Models\TransitCabutBulu;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransitCabutBuluController extends Controller
 {
@@ -12,8 +13,12 @@ class TransitCabutBuluController extends Controller
     public function index()
     {
         $i = 1;
-        $TransitPreCleaningStock = TransitCabutBulu::get();
+        $TransitPreCleaningStock = TransitCabutBulu::with('CabutBuluPengembalian');
         // return $PrmRawMOI;
+        if(Auth::user()->plant){
+            $TransitPreCleaningStock->where('tujuan_kirim',Auth::user()->plant);
+        }
+        $TransitPreCleaningStock=$TransitPreCleaningStock->get();
         return response()->view('CabutBulu.CabutBuluTransit.index', [
             'transit_pre_cleaning_stocks' => $TransitPreCleaningStock,
             'i' => $i,
