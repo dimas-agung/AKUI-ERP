db8e4366122a5a06ef61f6f92c7da5ab814e1607
dimas agung <agungdimas390@gmail.com>
2024-07-09 16:02:36 +0700

    fix bug

--- resources/views/Rambang/HcrKotorInput/index.blade.php
+++ resources/views/Rambang/HcrKotorInput/index.blade.php
@@ -359,7 +359,7 @@
                 start_date: start_date,
                 end_date: end_date,
             };
-            var url = '{{ route('PreCleaningInput.index') }}';
+            var url = '{{ route('InputHcrKotor.index') }}';
 
             // url = url.replace(':slug', slug);
             url = url + '?start_date=' + start_date + '&end_date=' + end_date;
