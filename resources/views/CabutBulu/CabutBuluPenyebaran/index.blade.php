f916f4848faed1bcb0db58c9f744c3b8767fea71
dimas agung <agungdimas390@gmail.com>
2024-07-11 08:55:22 +0700

    fix typo redirect filter

--- resources/views/CabutBulu/CabutBuluPenyebaran/index.blade.php
+++ resources/views/CabutBulu/CabutBuluPenyebaran/index.blade.php
@@ -238,7 +238,7 @@
                 start_date: start_date,
                 end_date: end_date,
             };
-            var url = '{{ route('PreCleaningInput.index') }}';
+            var url = '{{ route('CabutBuluPenyebaran.index') }}';
 
             // url = url.replace(':slug', slug);
             url = url + '?start_date=' + start_date + '&end_date=' + end_date;
--- resources/views/CabutHancuran/CabutHancuranPengembalian/index.blade.php
+++ resources/views/CabutHancuran/CabutHancuranPengembalian/index.blade.php
@@ -209,7 +209,7 @@
                 start_date: start_date,
                 end_date: end_date,
             };
-            var url = '{{ route('PreCleaningInput.index') }}';
+            var url = '{{ route('CabutHancuranPengembalian.index') }}';
 
             // url = url.replace(':slug', slug);
             url = url + '?start_date=' + start_date + '&end_date=' + end_date;
