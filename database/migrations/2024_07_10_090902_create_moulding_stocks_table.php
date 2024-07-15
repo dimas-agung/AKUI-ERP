0446fd031fc3fd9786c37fa2a1b0d9583116a992
dimas agung <agungdimas390@gmail.com>
2024-07-15 15:00:59 +0700

    fix duplicate

--- database/migrations/2024_07_10_090902_create_moulding_stocks_table.php
+++ database/migrations/2024_07_10_090902_create_moulding_stocks_table.php
@@ -11,21 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('moulding_stocks', function (Blueprint $table) {
-            $table->id();
-            $table->string('unit');
-            $table->string('nomor_job');
-            $table->string('nomor_batch');
-            $table->string('tujuan_kirim');
-            $table->string('job_order');
-            $table->float('berat_job');
-            $table->float('pcs_job');
-            $table->float('modal_nomor_job', 16, 4);
-            $table->float('total_modal_nomor_job', 16, 4);
-            $table->float('upah_operator', 16, 4);
-            $table->integer('status')->default(1)->comment('0 => STATUS_NON_AKTIF, 1 => STATUS_ON_STOCK, 2 => STATUS_ON_PROSES, 3 => STATUS_FINISHED');
-            $table->timestamps();
-        });
+        // Schema::create('moulding_stocks', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('unit');
+        //     $table->string('nomor_job');
+        //     $table->string('nomor_batch');
+        //     $table->string('tujuan_kirim');
+        //     $table->string('job_order');
+        //     $table->float('berat_job');
+        //     $table->float('pcs_job');
+        //     $table->float('modal_nomor_job', 16, 4);
+        //     $table->float('total_modal_nomor_job', 16, 4);
+        //     $table->float('upah_operator', 16, 4);
+        //     $table->integer('status')->default(1)->comment('0 => STATUS_NON_AKTIF, 1 => STATUS_ON_STOCK, 2 => STATUS_ON_PROSES, 3 => STATUS_FINISHED');
+        //     $table->timestamps();
+        // });
     }
 
     /**
